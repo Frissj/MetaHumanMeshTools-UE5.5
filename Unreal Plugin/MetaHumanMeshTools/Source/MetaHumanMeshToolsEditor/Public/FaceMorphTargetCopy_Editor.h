38b0d08c4a027227bf6e7d67fc32f8922575ea8e
Sam <49733753+Frissj@users.noreply.github.com>
2025-02-09 16:46:59 +0000

    Add files via upload

---
+++ Unreal Plugin/MetaHumanMeshTools/Source/MetaHumanMeshToolsEditor/Public/FaceMorphTargetCopy_Editor.h
@@ -0,0 +1,24 @@
+﻿// File: FaceMorphTargetCopy_Editor.h
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BlueprintEditorLibrary.h"
+#include "FaceMorphTargetCopy_Editor.generated.h"
+
+/**
+ * This class contains blueprint callable utility functions to copy morph target data
+ * from one MetaHuman face mesh to another.
+ * (Editor-only functionality.)
+ */
+UCLASS()
+class METAHUMANMESHTOOLSEDITOR_API UFaceMorphTargetCopy_Editor : public UBlueprintEditorLibrary
+{
+    GENERATED_BODY()
+
+public:
+    UFUNCTION(BlueprintCallable, Category = "MetaHuman Mesh Tools")
+    static void CopyFaceMorphTargets(USkeletalMesh* sourceMesh, USkeletalMesh* targetMesh);
+
+    UFUNCTION(BlueprintCallable, Category = "MetaHuman Mesh Tools")
+    static void CopyFaceMorphTargetsUsingReference(USkeletalMesh* originalSourceMesh, USkeletalMesh* vertexPositionReferenceMesh, USkeletalMesh* targetMesh);
+};
